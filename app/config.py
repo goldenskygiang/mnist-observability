07f3c254c206e2e5187fa7566214e3bbb1319872
Vu Truong Giang <giangthedev@outlook.com.vn>
2024-03-04 20:27:43 -0500

    init dirs

--- app/config.py
+++ app/config.py
@@ -1,12 +1,21 @@
 import os
+from typing import List
 import dotenv
 
 dotenv.load_dotenv()
 
+def init_dirs(dirs: List[str]):
+    for d in dirs:
+        if not(os.path.exists(d)):
+            os.mkdir(d)
+
 MONGODB_URL = os.environ.get('MONGODB_URL')
-MNIST_DATASET_DIR = os.environ.get('MNIST_DATASET_DIR')
 REDIS_URL = os.environ.get('REDIS_URL')
+
+MNIST_DATASET_DIR = os.environ.get('MNIST_DATASET_DIR')
 LOG_DIR = os.environ.get('LOG_DIR')
+
+init_dirs([MNIST_DATASET_DIR, LOG_DIR])
 
 GRID_SEARCH_LIMIT = 16
 
